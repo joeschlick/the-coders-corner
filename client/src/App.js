abe285339ee37ad878bcee6cb9a1499133f97a5b
Brian Praseuth <brianpraseuth@gmail.com>
2020-11-21 12:02:16 -0800

    Created components folder/files and Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Login from "./components/Login";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+import Resources from "./components/Resources";
+import Chat from "./components/Chat";
+import Editor from "./components/Editor";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <CssBaseline />
+    <Navbar />
+    <Route exact path="/" component={Login}/>
+    <Route exact path="/feed" component={Feed}/>
+    <Route exact path="/profile" component={Profile}/>
+    <Route exact path="/resources" component={Resources}/>
+    <Route exact path="/chat" component={Chat}/>
+    <Route exact path="/editor" component={Editor}/>
     </div>
   );
 }
