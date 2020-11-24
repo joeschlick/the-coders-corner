a385441739d90964503e2d6fe192b0d930de3291
kdoh52 <kdoh52@gmail.com>
2020-11-23 16:39:24 -0800

    working and styled feed

--- client/src/components/NewsHeader.js
+++ client/src/components/NewsHeader.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from "axios";
 import NewsPost from "./NewsPost";
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
 
-export default function NewsHeader() {
-    let newsArray = [];
+const useStyles = makeStyles(theme => ({
+    title:{
+        marginTop: 25,
+        marginBottom: 10
+    },
+    headline: {
+        marginBottom: 30
+    }
+}))
+
+
+function NewsHeader() {
+    // let newsArray = [];
+    let classes = useStyles();
+    const [newsArray, setNewsArray] = useState([])
+
     const options = {
       method: 'GET',
       url: 'https://newscatcher.p.rapidapi.com/v1/search_free',
       params: {q: 'Web Development', media: 'True', lang: 'en'},
       headers: {
-        // 'x-rapidapi-key': '12d5ec1238msh592efbbc1c917e7p1b499ejsnc4830fc60542',
-        "x-rapidapi-key": "2b07b6ec73mshd7f568f32fa4427p1efeb3jsnf11a6a1c6510",
+        'x-rapidapi-key': '12d5ec1238msh592efbbc1c917e7p1b499ejsnc4830fc60542',
+        // "x-rapidapi-key": "2b07b6ec73mshd7f568f32fa4427p1efeb3jsnf11a6a1c6510",
         'x-rapidapi-host': 'newscatcher.p.rapidapi.com'
       }
     };
@@ -27,31 +43,40 @@ export default function NewsHeader() {
         // let newsArray = [];
         for (let i = 0; i < 5; i++) {
             console.log("hi")
-            newsArray.push({
-                title: response.articles[i].title,
-                summary: response.articles[i].summary,
-                date: response.articles[i].published_date
-            })
+            setNewsArray(response.articles)
+            // newsArray.push({
+            //     title: response.articles[i].title,
+            //     summary: response.articles[i].summary,
+            //     date: response.articles[i].published_date
+            // })
         }
         // console.log(newsArray);
     }
     
     console.log("here", newsArray);
+
     return (
-        <div>
-            <h1>Newsfeed</h1>
-            <p>Surf the latest news in web development!</p>
-            {newsArray.map(item => {
-                return (
-                    <NewsPost
-                        title={item.title}
-                        summary={item.summary}
-                        date={item.date}
-                    />
-                )
-            })}
-        </div>
+        <ThemeProvider>
+            <div>
+                <Typography className={classes.title} variant='h2' align="center">
+                    Newsfeed
+                </Typography>
+                <Typography className={classes.headline} variant='h5' align="center">
+                    Surf the latest news in web development!
+                </Typography>
+
+                {newsArray.slice(0, 10).map(item => {
+                    return (
+                        <NewsPost
+                            title={item.title}
+                            summary={item.summary}
+                            date={item.date}
+                        />
+                    )
+                })}
+            </div>
+        </ThemeProvider>
     )
 }
 
-// export default NewsHeader;
+export default NewsHeader;
--- client/src/components/NewsPost.js
+++ client/src/components/NewsPost.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    title:{
+        marginBottom: 10
+    },
+    containers: {
+        background: "#CACACA",
+        padding: 25,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingBottom: 35,
+        margin: 20,
+        borderRadius: 25
+    }
+}))
+
 
 function NewsPost(props) {
+    let classes = useStyles();
     return (
-        <div>
-            <h1>post</h1>
-            <h1>{props.title}</h1>
-            <p>{props.summary}</p>
+        <div className={classes.containers}>
             <p>{props.date}</p>
+            <Typography className={classes.title} variant="h6">
+                {props.title}
+            </Typography>
+            <Typography variant="body2">
+                {props.summary}
+            </Typography>
         </div>
     )
 }
