8d31f37c2a5bbafb1f2fc2e9f69bd6fa36938ed0
Brian Praseuth <brianpraseuth@gmail.com>
2020-12-01 21:52:22 -0800

    Hackhup and profile fixed.

--- client/src/components/HackHub.js
+++ client/src/components/HackHub.js
@@ -1,7 +1,14 @@
-import React, { useContext } from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import UserContext from '../context/UserContext';
+import {  
+    Button, 
+    TextField, 
+    Typography
+} from '@material-ui/core';
+import API from "../utils/API";
+
+
 
 const useStyles = makeStyles(theme => ({
     title:{
@@ -13,9 +20,49 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-export default function Profile() {
+export default function HackHub() {
     let classes = useStyles();
+    const [users, setUsers] = useState([]);
     const { userData } = useContext(UserContext);
+    const [formObject, setFormObject] = useState([]);
+
+    let getUser = localStorage.getItem("user");
+    let userInfo = JSON.parse(getUser);
+    
+
+    function loadUsers(){
+        API.getUsers()
+            .then((res) => {
+                setUsers(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    function handleInputChange(e){
+        const { name, value } = e.target;
+        setFormObject({ ...formObject, [name]: value });
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault();
+        if(formObject.post){
+            API.updateUser(userInfo.user._id, {
+                posts:[{
+                    post: formObject.post,
+                    likes: 0
+                  }]
+            })
+            .then((res, req) => {
+                console.log(formObject.post);
+                console.log(res.data);
+                loadUsers();
+            })
+            .catch((err) => console.log(err));
+        }
+    }
+
     return (
         <div>
             {console.log(userData)}
@@ -25,6 +72,32 @@ export default function Profile() {
             <Typography className={classes.headline} variant='h5' align="center">
                 See what the hubbub is about!
             </Typography>
+            <Typography variant="h4">
+                {userInfo.user.userName}
+            </Typography>
+            <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="post"
+                        onChange={handleInputChange}
+                        name="post"
+                        autoFocus
+                        label="Write post here"
+                    />
+                <Button
+                    fullWidth
+                    className={classes.submit}
+                    variant="contained" 
+                    disabled={!(formObject.post)} 
+                    onClick={handleFormSubmit}
+                >
+                    Submit
+                </Button>
+
+                </form>
         </div>
     )
 }
