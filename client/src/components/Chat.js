7d2004a3c6c9e2a50f982ea1591216798ff202c9
kdoh52 <kdoh52@gmail.com>
2020-12-02 14:12:46 -0800

    chat styling/username

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,12 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import io from "socket.io-client";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles'
 
 const socket = io.connect('http://localhost:3001/');
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        '& label.Mui-focused': {
+            color: 'green',
+        },
+        '& .MuiInput-underline:after': {
+        borderBottomColor: 'green',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'red',
+            },
+            '&:hover fieldset': {
+                borderColor: 'yellow',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'green',
+            },
+        },
+    },
     title:{
         marginTop: 25,
         marginBottom: 10
@@ -18,26 +38,65 @@ const useStyles = makeStyles(theme => ({
         display: "flex"
     },
     form: {
-        margin: 50,
-        marginLeft: 200,
-        backgroundColor: '#e0e0e0',
-        padding: 30,
-        borderRadius: 20
+        width: '70vh',
+        height: '28vh',
+        display: 'flex',
+        flexDirection: 'column',
+        borderRadius: 20,
+        backgroundColor: '#457b9d',
+        // backgroundColor: '#737578',
+        marginLeft: "40px",
+        marginRight: "20px",
+        padding: "30px"
+    },
+    sendTitle: {
+        color: "#E0E0E0"
     },
     chatLog: {
-        margin: 50,
-        backgroundColor: '#e0e0e0',
-        padding: 30,
+        width: '100vh',
+        height: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '3px 2px 5px rgba(0,0,0,.26)',
         borderRadius: 20,
-        width: 400
+        backgroundColor: '#E0E0E0',
+
+    },
+    chatLogTitle: {
+        margin: 0,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        // borderRadius: 20,
+        color: "white",
+        backgroundColor: "#457b9d",
+        padding: "30px"
+    },
+    sendBtn: {
+        width: '50px'
+    },
+    messageField: {
+        marginTop: '10px',
+        marginBottom: '15px',
+    },
+    message: {
+        margin: '7px'
+    },
+    messageContainer: {
+        padding: "20px"
+    },
+    userName: {
+        fontWeight: 'bold'
     }
-}))
+}));
 
 function Chat() {
     let classes = useStyles();
     const [state, setState] = useState({message: '', name: ''})
     const [chat, setChat] = useState([])
+    // const [formObject, setFormObject] = useState([]);
 
+    let getUser = localStorage.getItem("user");
+    let userInfo = JSON.parse(getUser);
 
     useEffect(() => {
         socket.on('message', ({name, message}) => {
@@ -52,16 +111,17 @@ function Chat() {
     const onMessageSubmit = event => {
         event.preventDefault();
 
-        const {name, message} = state
+        // const {name, message} = state
+        const {message} = state
+        const name = userInfo.user.userName
         socket.emit('message', {name, message})
         setState({message: '', name})
     }
 
-
     const renderChat = () => {
         return chat.map(({name, message}, index) => (
             <div key={index}>
-                <h3>{name}: <span>{message}</span></h3>
+                <Typography variant="subtitle1" className={classes.message}><span className={classes.userName}>{name}:</span> <span>{message}</span></Typography>
             </div>
         ))
     }
@@ -75,29 +135,36 @@ function Chat() {
                 Message fellow developers!
             </Typography>
             <div className={classes.formContainer}>
-                <form className={classes.form} onSubmit={onMessageSubmit}>
-                    <div className="name-field">
+                <form className={classes.form}>
+                    <Typography className={classes.sendTitle} variant='h6'>
+                        Send a Message
+                    </Typography>
+                    {/* <div className="name-field">
                         <TextField
                             name="name"
                             onChange={e => onTextChange(e)}
                             value={state.name}
+                            variant="outlined"
                             label="Name"
                         />
-                    </div>
-                    <div className="message-field">
+                    </div> */}
+                    <div className={classes.messageField}>
                         <TextField
                             name="message"
                             onChange={e => onTextChange(e)}
                             value={state.message}
+                            variant="outlined"
                             label="Message"
                         />
                     </div>
-                    <button>Send</button>
+                    <Button className={classes.sendBtn} variant="contained" onClick={onMessageSubmit}>Send</Button>
                 </form>
                 
                 <div className={classes.chatLog}>
-                    <h1>Live Message Board</h1>
-                    {renderChat()}
+                    <Typography className={classes.chatLogTitle} variant="h5">Live Message Board</Typography>
+                    <div className={classes.messageContainer}>
+                        {renderChat()}
+                    </div>
                 </div>  
             </div>
         </div>
